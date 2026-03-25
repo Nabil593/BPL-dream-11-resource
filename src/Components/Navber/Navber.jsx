ac25e03aa5e02665b623f94430402f07f96d0af8
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-03-25 16:27:02 +0600

    BPL-DREAM

---
+++ src/Components/Navber/Navber.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navber = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <img src="" alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navber;
